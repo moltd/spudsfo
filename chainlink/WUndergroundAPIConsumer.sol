05da3952f9e2c1f9ee6f4cce1150e67a803285c0
Jonathan Bruce <jonathan.m.bruce@gmail.com>
2022-01-11 20:17:16 -0800

    Update WUndergroundAPIConsumer.sol
    Corrected contract name

--- chainlink/WUndergroundAPIConsumer.sol
+++ chainlink/WUndergroundAPIConsumer.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
-contract APIConsumer is ChainlinkClient {
+contract WUndergroundAPIConsumer is ChainlinkClient {
     using Chainlink for Chainlink.Request;
   
     uint256 public precipRate;
